87eb738199b2d77029ae37775c490b5c737b829a
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-09-18 18:05:21 +0530

    zip func

---
+++ zip func.py
@@ -0,0 +1,14 @@
+s1={1,2,3,4}
+s2={2,4,6,8}
+s3=list(zip(s1,s2))
+print(s3)
+
+list1=[100,200,300]
+list2=[1,2,3]
+for x,y in zip(list1,list2[::-1]):
+    print(x,y)
+
+stocks=['relience','infosys','tcs']
+price=[000,2500,3000]
+new_dict={stocks:price for stocks,price in zip(stocks,price)}
+print('\n{}'.format(new_dict))
